ec539ab636d2e91244b916517d693667250ecc0f
BilgeGokhan <47634641+BilgeGokhan@users.noreply.github.com>
2020-01-22 16:43:47 +0300

    First Commit
    My First WebApi example.

---
+++ FirstWebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,51 @@
+﻿using FirstWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FirstWebApi.Controllers
+{
+    [RoutePrefix("api/employee")]
+
+    public class EmployeeController : ApiController
+    {       
+        List<Employee> empList = new List<Employee>()
+        {
+            //Bu Controller testi için WebApiConfig üzerinde routeTemplate => {action} ibaresini silmelisin. 
+
+            new Employee{Id=1,Name="Emp1"},
+            new Employee{Id=2,Name="Emp2"},
+            new Employee{Id=3,Name="Emp3"}
+        };
+
+        [Route("")]
+        public IEnumerable<Employee> Get()
+        {
+            return empList;
+        }
+
+        [Route("{id}")]
+        public Employee Get(int id)
+        {
+            return empList.FirstOrDefault(e => e.Id == id);
+        }
+
+        [Route("{id}/tasks")]
+        public IEnumerable<string> GetEmployeeTasks(int id) {
+            switch (id)
+            {
+                case 1:
+                    return new List<string> { "Task 1-1", "Task 1-2" };
+                case 2:
+                    return new List<string> { "Task 2-1", "Task 2-2" };
+                case 3:
+                    return new List<string> { "Task 3-1", "Task 3-2" };
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ FirstWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FirstWebApi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        static List<string> myList = new List<string>()
+        {
+            "Bilgem",
+            "Gülsevinim",
+        };
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Degerler()
+        {
+            return myList;
+        }
+
+        // GET api/values/5
+        [HttpGet]
+        public string DegerGetir(int id)
+        {
+            return myList[id];
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void DegerEkle([FromBody]string value)
+        {
+            myList.Add(value);
+        }
+
+        // PUT api/values/5
+        [HttpPut]
+        public void DegerGuncelle(int id, [FromBody]string value)
+        {
+            myList[id] = value;
+        }
+
+        // DELETE api/values/5
+        [HttpDelete]
+        public void DegerSil(int id)
+        {
+            myList.RemoveAt(id);
+        }
+    }
+}
